461d57186125891ba0f33abb67260e8f0edecf6f
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-13 21:40:02 -0300

    feat: add normalize for css reset

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider, FontLoader } from '@gympass/yoga';
+import { Normalize } from 'styled-normalize';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
       <FontLoader />
+      <Normalize />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
