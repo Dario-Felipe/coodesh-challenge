a0288e391e0a47f699ad10613c11d873882e1dee
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-13 21:40:30 -0300

    feat(header): add new header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Container, Row, Col } from '@gympass/yoga';
 import { useParams } from 'react-router-dom';
+import Header from '../components/Header';
 
 const home = () => {
   const { page } = useParams();
 
   return (
-    <Container>
-      <Row>
-        <Col xxs={12}>
-          <h1>Olá mundo! pagina: {page}</h1>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <Row>
+          <Col xxs={12}>
+            <h1>Página: {page}</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
