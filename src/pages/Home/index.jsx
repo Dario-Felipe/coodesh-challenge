8c6a702dd5485a1e6560a85846f052f6f2b74998
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-15 16:55:24 -0300

    feat(home): add new input and filter functions

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Box } from '@gympass/yoga';
+import { Container, Row, Col, Box, AutoComplete } from '@gympass/yoga';
 import { useParams, useHistory } from 'react-router-dom';
 import { Section, Content, Title, TableContainer } from './style';
 import Header from '../../components/Header';
@@ -12,10 +12,24 @@ const Home = () => {
   const { page } = useParams();
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(false);
   const history = useHistory();
+
+  const cleanInput = () => {
+    const closeButton = document.querySelector(
+      '#searchInput>div>div>fieldset>div',
+    );
+
+    if (closeButton) {
+      closeButton.click();
+    }
+  };
 
   const loadUsers = async () => {
     setIsLoading(true);
+    cleanInput();
+    setFilteredUsers(false);
 
     if (Number.isNaN(Number(page))) {
       history.push('/404');
@@ -35,6 +49,40 @@ const Home = () => {
     }
   };
 
+  const searchUser = (value) => {
+    setIsLoading(true);
+    setSearchInput(value);
+
+    const filtered = users.filter(
+      (item) =>
+        item.name.first
+          .concat(' ', item.name.last)
+          .toLowerCase()
+          .includes(value) ||
+        item.location.country.toLowerCase().includes(value) ||
+        item.gender.toLowerCase() === value,
+    );
+
+    if (filtered.length !== 0) {
+      setFilteredUsers(filtered);
+    } else {
+      setFilteredUsers(false);
+    }
+
+    setIsLoading(false);
+  };
+
+  const generateOptions = (array) => {
+    const options = [
+      ...array.map((item) => `${item.name.first} ${item.name.last}`),
+      ...new Set(array.map((item) => item.location.country)),
+      'Male',
+      'Female',
+    ];
+
+    return options;
+  };
+
   useEffect(() => {
     loadUsers();
   }, [page]);
@@ -48,11 +96,33 @@ const Home = () => {
             <Col xxs={12}>
               <Content>
                 <Title>&#128269; Search patient &#128270;</Title>
+                <Box
+                  id="searchInput"
+                  padding="medium"
+                  backgroundColor="white"
+                  borderRadius={20}
+                  width="70%"
+                  marginBottom="medium"
+                >
+                  <AutoComplete
+                    value={searchInput}
+                    label="Search here"
+                    full
+                    onChange={(value) => searchUser(value.toLowerCase())}
+                    options={generateOptions(users)}
+                  />
+                </Box>
                 {isLoading ? (
                   <Loading />
                 ) : (
                   <TableContainer>
-                    <Table users={users} />
+                    {searchInput !== '' && !filteredUsers && (
+                      <Box paddingBottom="small">
+                        <h1>Patient not found &#128531;</h1>
+                        <span>Others bellow:</span>
+                      </Box>
+                    )}
+                    <Table users={filteredUsers || users} />
                     <Box padding="medium">
                       <PageControl page={page} />
                     </Box>
