646f1f0b6404152c7fa03d6b8223d30bacf2535d
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-14 23:14:35 -0300

    feat(modal): add new modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Section, Content, Image, List, CloseButton } from './style';
+
+const Modal = ({ user, handle }) => (
+  <Section>
+    <Content>
+      <Image src={user.picture.large} alt={`Picture of ${user.name.first}`} />
+      <h1>{user.name.first}</h1>
+      <List>
+        <li>
+          <strong>ID: </strong>
+          {user.id.name ? `${user.id.name}${user.id.value}` : 'Empety'}
+        </li>
+        <li>
+          <strong>Name: </strong>
+          {user.name.title}. {user.name.first} {user.name.last}
+        </li>
+        <li>
+          <strong>Email: </strong>
+          {user.email}
+        </li>
+        <li>
+          <strong>Gender: </strong>
+          {user.gender}
+        </li>
+        <li>
+          <strong>Birth day: </strong>
+          {user.dob.date.slice(0, 10)} ({user.dob.age} years old)
+        </li>
+        <li>
+          <strong>Phone number: </strong>
+          {user.phone} and {user.cell}
+        </li>
+        <li>
+          <strong>Address: </strong>
+          {user.location.country} - {user.location.state} - {user.location.city}
+        </li>
+      </List>
+      <CloseButton onClick={() => handle(false)} type="button" secondary>
+        Close
+      </CloseButton>
+    </Content>
+  </Section>
+);
+
+export default Modal;
---
+++ src/components/Modal/style.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { theme, Button } from '@gympass/yoga';
+import { media } from '@gympass/yoga-helpers';
+import colors from '../../tokens/colors';
+
+export const Section = styled.section`
+  align-items: center;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  left: ${theme.spacing.zero};
+  position: fixed;
+  top: ${theme.spacing.zero};
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  align-items: center;
+  background: ${colors.white};
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  padding: ${theme.spacing.medium}px;
+
+  h1 {
+    margin: ${theme.spacing.zero};
+  }
+
+  ${media.xxs`
+    height: 400px;
+    width: 80%;
+  `}
+
+  ${media.xs`
+    height: 478px;
+  `}
+
+  ${media.sm`
+    height: 450px;
+    width: 50%;
+  `}
+
+  ${media.lg`
+    height: 450px;
+    width: 550px;
+  `}
+`;
+
+export const Image = styled.img`
+  border-radius: 120px;
+  border: 4px solid ${colors.primary};
+  position: absolute;
+  top: -44px;
+  width: 130px;
+`;
+
+export const List = styled.ul`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing.xsmall}px;
+  justify-content: center;
+  list-style: none;
+  padding: ${theme.spacing.zero};
+
+  ${media.xxs`
+    li {
+      font-size: ${theme.fontSizes.xsmall}px;
+
+      strong {
+        font-size: ${theme.fontSizes.small}px;
+      }
+    }
+  `}
+
+  ${media.xs`
+    li {
+      font-size: ${theme.fontSizes.small}px;
+
+      strong {
+        font-size: ${theme.fontSizes.medium}px;
+      }
+    }
+  `}
+`;
+
+export const CloseButton = styled(Button)`
+  background: ${colors.dark};
+  bottom: ${theme.spacing.medium}px;
+  position: absolute;
+`;
